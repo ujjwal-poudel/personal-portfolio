3ac14d3aa8caffd60018f3f97dcfeebde537575c
UjjwalPoudel2003 <upoudel1@my.centennialcollege.ca>
2023-09-24 00:45:01 -0400

    express js initial commit

---
+++ config/express.js
@@ -0,0 +1,33 @@
+// Importing the dependencies
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const compress = require('compress');
+
+module.exports = () => {
+    const app = express();
+
+    if(process.env.NODE_ENV === 'development'){
+        app.use(morgan('Dev'));
+    } else if(process.env.NODE_ENV === 'production'){
+        app.use(morgan('Prod'));
+        app.use(compress());
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+    app.use(methodOverride());
+
+    app.set('views', './app/views');
+    app.set('view engine', 'ejs');
+    app.use('/', require('../app/routes/index.server.routes.js'));
+
+    app.use(express.static('./node_modules'));
+
+    return app;
+};
